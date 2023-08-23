704f3c92d1b93073398f5d18e1c4a42a2ef5a5c8
Adriana Barrera <barreraadriana33@gmail.com>
2023-08-22 23:28:59 -0500

    Agregue los menus de desktop, hamburguesa y carrito

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const menuHamburguesa = document.querySelector('.menu');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburguesa.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    //Si el Aside esta abierto, hay que cerrarlo
+    if(!isAsideClosed){
+        aside.classList.toggle('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inacive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    //Si el mobileMenu esta abierto, hay que cerrarlo
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+    
+    aside.classList.toggle('inactive');
 }
